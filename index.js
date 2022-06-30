0ce0707ac1f8a2f696114e9122278061d7abf649
unknown <afwan.rizky96@gmail.com>
2022-06-30 12:41:35 +0700

    make it better

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const db = mysql.createConnection({
   host: 'db4free.net',
   user: 'batagor',
   password: 'sayasukabatagor',
-  database: 'member2',
+  database: 'employee_crud',
 });
 
 app.get('/', (req, res) => {
